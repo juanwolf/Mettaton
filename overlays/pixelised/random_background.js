6f4031e4182b416aa946f5a632f0cf2775f728dc
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2020-12-06 18:44:03 +0000

    feat: Add overlays

---
+++ overlays/pixelised/random_background.js
@@ -0,0 +1,16 @@
+//by @nodws
+/*
+  Using trianglify.js and d3.js library
+*/
+//Click to change bg
+function changeBg() {
+  var rn = Math.floor((Math.random() * 150) + 60);
+  var rs = Math.floor((Math.random() * 11) + 4);
+  var t = new Trianglify({
+    x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+  });
+  var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+  document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+}
